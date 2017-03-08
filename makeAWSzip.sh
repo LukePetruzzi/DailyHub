c323f73311ab144787a9b835cdd40aa29bc44591
Luke Petruzzi <lukepetruzzi@gmail.com>
2017-03-08 18:21:06 -0500

    Wrote a script to make and upload our code to the lambda function via AWS CLI console

--- makeAWSzip.sh
+++ makeAWSzip.sh
@@ -1,2 +1,10 @@
+#!/bin/sh
 
+# install required packages in this directory
+pip install --upgrade requests -t ./
 
+# create a zip file of the current directory to upload to AWS
+zip -r ./AWSLambdaPackage.zip *
+
+# upload the deployment package to the Lambda function
+aws lambda update-function-code --function-name UpdateMasterDictionaryPeriodically --zip-file fileb://AWSLambdaPackage.zip
