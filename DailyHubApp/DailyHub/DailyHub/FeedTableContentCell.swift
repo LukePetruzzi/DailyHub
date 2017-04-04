d912731ec56e87c8c2e8e93b89e13b6849ade452
Joe Salter <jcsalter@princeton.edu>
2017-04-03 21:53:01 -0400

    Custom tableviewcell cell started for content

---
+++ DailyHubApp/DailyHub/DailyHub/FeedTableContentCell.swift
@@ -0,0 +1,32 @@
+//
+//  FeedTableContentCell.swift
+//  DailyHub
+//
+//  Created by Joe Salter on 4/3/17.
+//  Copyright © 2017 Luke Petruzzi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedTableContentCell: UITableViewCell {
+    
+    var author:UILabel?
+    var title:UILabel?
+    var url:String?
+    var thumbnail:String?
+    var desc:String?
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private override func prepareForReuse() {
+        super.prepareForReuse()
+        gifView!.image = nil
+    }
+}
