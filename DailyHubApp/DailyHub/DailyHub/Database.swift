661dfb0100017e71e7f24e1085813a5d1c6f6096
Joe Salter <jcsalter@princeton.edu>
2017-04-03 22:00:50 -0400

    Database stuff moved to its own file

---
+++ DailyHubApp/DailyHub/DailyHub/Database.swift
@@ -0,0 +1,55 @@
+//
+//  Database.swift
+//  DailyHub
+//
+//  Created by Joe Salter on 4/3/17.
+//  Copyright © 2017 Luke Petruzzi. All rights reserved.
+//
+
+import Foundation
+import AWSS3
+import AWSDynamoDB
+import AWSSQS
+import AWSSNS
+import AWSCognito
+
+class Database {
+    
+    class func getDatabaseInfo()
+    {
+    
+        let dynamodb = AWSDynamoDB.default()
+        let req = AWSDynamoDBGetItemInput()
+        req?.tableName = "MainStorageFeed"
+        let value:AWSDynamoDBAttributeValue = AWSDynamoDBAttributeValue()
+        // this needs to be the current date in ISO
+        value.s = getFormattedESTDate()
+        req?.key = ["Date":value]
+        
+        dynamodb.getItem(req!).continueWith { (task) -> Any? in
+            if let error = task.error {
+                print("Error occurred: \(error)")
+                return nil
+            }
+            let output = task.result!.item
+            print("VALUE: ",output?.values.first?.s ?? "TABLE VALUE DOES NOT EXIST OR TABLE IS EMPTY!!!")
+            
+
+            return nil
+        }
+        
+        
+    }
+    
+    func getFormattedESTDate() -> String{
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
+        // Eastern Standard Time
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(abbreviation: "EST")
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: Date())
+    }
+
+    
+}
--- DailyHubApp/DailyHub/DailyHub/FeedViewController.swift
+++ DailyHubApp/DailyHub/DailyHub/FeedViewController.swift
@@ -6,25 +6,20 @@
 //  Copyright © 2017 Luke Petruzzi. All rights reserved.
 //
 
-import AWSS3
-import AWSDynamoDB
-import AWSSQS
-import AWSSNS
-import AWSCognito
-
 import UIKit
-
+import Foundation
 
 class FeedViewController: UIViewController {
     
-    var count = 0
+    var tableView: UITableView = {
+        var tv = UITableView()
+        return tv
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(red:0.29, green: 0.29, blue:0.29, alpha: 1.0)
-
-        getDatabaseInfo()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -33,45 +28,5 @@ class FeedViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    private func getDatabaseInfo()
-    {
-        
-        
-        let dynamodb = AWSDynamoDB.default()
-        let req = AWSDynamoDBGetItemInput()
-        req?.tableName = "MainStorageFeed"
-        let value:AWSDynamoDBAttributeValue = AWSDynamoDBAttributeValue()
-        // this needs to be the current date in ISO
-        value.s = getFormattedESTDate()
-        req?.key = ["Date":value]
-        
-        dynamodb.getItem(req!).continueWith { (task) -> Any? in
-            if let error = task.error {
-                print("Error occurred: \(error)")
-                return nil
-            }
-            let output = task.result!.item
-            print("VALUE: ",output?.values.first?.s ?? "TABLE VALUE DOES NOT EXIST OR TABLE IS EMPTY!!!")
-            
-            self.count += 1
-            print("COUNT: ", self.count)
-            return nil
-        }
-        
-        
-    }
-    
-    private func getFormattedESTDate() -> String{
-        let formatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .iso8601)
-        // Eastern Standard Time
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(abbreviation: "EST")
-        formatter.dateFormat = "yyyy-MM-dd"
-        return formatter.string(from: Date())
-    }
-
-
 }
 
