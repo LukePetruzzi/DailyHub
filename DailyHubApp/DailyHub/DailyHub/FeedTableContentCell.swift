49620258fa140639f9b2375d5b6c738ea40ba095
Joe Salter <jcsalter@princeton.edu>
2017-04-11 00:14:16 -0400

    using sdwebimage to display image

--- DailyHubApp/DailyHub/DailyHub/FeedTableContentCell.swift
+++ DailyHubApp/DailyHub/DailyHub/FeedTableContentCell.swift
@@ -25,44 +25,26 @@ class FeedTableContentCell: UITableViewCell {
         titleLabel = UILabel()
         descLabel = UILabel()
         imgView = UIImageView()
-        imgView?.contentMode = .scaleAspectFit
-
-//        titleLabel = UILabel(frame: CGRect(x: 5, y: 5, width: UIScreen.main.bounds.width - 10, height: 25))
-//        authorLabel = UILabel(frame: CGRect(x: 5, y: 30, width: UIScreen.main.bounds.width - 10, height: 15))
-//        descLabel = UILabel(frame: CGRect(x: 5, y: 40, width: UIScreen.main.bounds.width - 10, height: 100))
+        imgView?.contentMode = .scaleAspectFill
+        imgView?.sd_setShowActivityIndicatorView(true)
+        imgView?.sd_setIndicatorStyle(.gray)
+        imgView?.layer.cornerRadius = 5
+        imgView?.clipsToBounds = true
+        imgView?.layoutIfNeeded()
                 
         titleLabel?.font = UIFont(name: "Avenir-Black", size: 16)
-//        titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
         
-        authorLabel?.font = authorLabel?.font.withSize(11)
+        authorLabel?.font = UIFont(name: "Avenir", size: 11)
         authorLabel?.textColor = UIColor.gray
         
+        descLabel?.font = UIFont(name: "Avenir", size: 12)
         descLabel?.lineBreakMode = .byWordWrapping
         descLabel?.numberOfLines = 0
-//
-//        titleLabel?.backgroundColor = UIColor(red:0.1, green: 0.78, blue:0.78, alpha: 1.0)
-//        authorLabel?.backgroundColor = UIColor(red:0.4, green: 0.78, blue:0.78, alpha: 1.0)
-//        descLabel?.backgroundColor = UIColor(red:1.0, green: 0.78, blue:0.78, alpha: 1.0)
-//        imgView?.backgroundColor = UIColor(red:0.78, green: 0.78, blue:0.78, alpha: 1.0)
-        
+
         self.addSubview(titleLabel!)
         self.addSubview(authorLabel!)
         self.addSubview(descLabel!)
         self.addSubview(imgView!)
-        
-//        let verticalConstraint1 = NSLayoutConstraint(item: authorLabel!, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: titleLabel!, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
-//        let verticalConstraint2 = NSLayoutConstraint(item: descLabel!, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: authorLabel!, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
-//        let verticalConstraint3 = NSLayoutConstraint(item: imgView!, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: descLabel!, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
-//
-//        self.contentView.addConstraints([verticalConstraint1, verticalConstraint2, verticalConstraint3])
-        
-//        let views: [String: UIView] = ["t": titleLabel!, "a": authorLabel!, "d": descLabel!, "i": imgView!]
-//        var constraints: [NSLayoutConstraint] = []
-//        
-//        constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|[t]-10-[a]-10-[d]-10-[i]|", options: [], metrics: nil, views: views)
-//        
-//        NSLayoutConstraint.activate(constraints)
-//
     }
 
     required init?(coder aDecoder: NSCoder) {
