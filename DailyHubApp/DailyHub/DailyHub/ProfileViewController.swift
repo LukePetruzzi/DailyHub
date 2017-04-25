cdb7f88fe09b887e68fb7dde6138576e79385e1e
Luke Petruzzi <lukepetruzzi@gmail.com>
2017-04-24 21:36:24 -0400

    fixed problem with logout, started creating profile controller

--- DailyHubApp/DailyHub/DailyHub/ProfileViewController.swift
+++ DailyHubApp/DailyHub/DailyHub/ProfileViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import SDWebImage
 
 class ProfileViewController: UIViewController {
     
+    var profileImageView: UIImageView!
+    var profileName: UILabel!
     var logoutButton: UIButton!
     
     
@@ -29,15 +33,54 @@ class ProfileViewController: UIViewController {
     
     internal func arrangeUI()
     {
-        let frame = CGRect(x: 0, y: 200, width: self.view.bounds.width, height: 50)
-        self.logoutButton = UIButton(frame: frame)
-        logoutButton?.contentVerticalAlignment = .bottom
-        logoutButton?.contentHorizontalAlignment = .center
+        // profile image view
+        profileImageView = UIImageView(frame: CGRect(x: self.view.center.x - 25, y: 50, width: 50, height: 50))
         
-        logoutButton?.backgroundColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 1)
-        logoutButton?.setTitle("Logout", for: .normal)
-        logoutButton?.addTarget(self, action: #selector(logout), for: .touchUpInside)
-        self.view.addSubview(logoutButton!)
+        var userId:String? = nil
+        
+        let waitGroup = DispatchGroup()
+        waitGroup.enter()
+        FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, relationship_status"]).start(completionHandler: {(connection, result, error) -> Void in
+            if ((error) != nil)
+            {
+                print("Error: \(error)")
+            }
+            else{
+                let res = result as! [String:AnyObject]
+                userId = res["id"] as! String?
+            }
+            
+            // leave dispatch group when finished
+            waitGroup.leave()
+        })
+        
+        waitGroup.notify(queue: .main) {
+            if (userId != nil)
+            {
+                let url =  URL(string: "http://graph.facebook.com/\(userId)/picture?type=large")
+                self.profileImageView.sd_setImage(with: url)
+                self.profileImageView.backgroundColor = UIColor.red
+                print("ASS: \(url?.absoluteString)")
+            }
+        }
+        
+        
+        
+        
+        
+        
+        // logout button
+        logoutButton = UIButton(frame: CGRect(x: 0, y: 200 , width: self.view.bounds.width, height: 50))
+        logoutButton.contentVerticalAlignment = .bottom
+        logoutButton.contentHorizontalAlignment = .center
+        logoutButton.backgroundColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 1)
+        logoutButton.setTitle("Logout", for: .normal)
+        logoutButton.addTarget(self, action: #selector(logout), for: .touchUpInside)
+        
+        
+        // add all the subviews
+        self.view.addSubview(logoutButton)
+        self.view.addSubview(profileImageView)
     }
     
     internal func logout(button: UIButton!)
